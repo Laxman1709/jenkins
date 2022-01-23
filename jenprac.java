7fc89f1dd0f945a70b7eefe43d33ff38cf107d01
salgaonkar laxman <laxmansalgaonkar786@gamil.com>
2022-01-23 11:29:13 +0530

    done

---
+++ jenprac.java
@@ -0,0 +1,23 @@
+package jenkins;
+
+public class jenprac {
+
+	public static void main(String[] args) {
+		String s="madam";
+		if(palindrome(s)) {
+			System.out.println("palindrome");
+		}
+		else {
+			System.out.println("Not a palindrome");
+		}
+			
+	}
+	private static boolean palindrome(String s) {
+		StringBuilder b=new StringBuilder(s);
+		if(b.reverse().toString().equals(s)) {
+			return true;
+		}
+		return false;
+	}
+
+}
